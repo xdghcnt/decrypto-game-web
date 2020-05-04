1bd2ee2ee5e9d048db647c36b04855da4d036355
xdghcnt <xd-mail@ya.ru>
2020-05-04 13:01:55 +0300

    Public rooms

--- public/app.jsx
+++ public/app.jsx
@@ -1040,7 +1040,7 @@ class Game extends React.Component {
 
                         <div className="side-buttons">
                             <i onClick={() => this.socket.emit("set-room-mode", false)}
-                               className="material-icons exit settings-button">home</i>
+                               className="material-icons exit settings-button">store</i>
                             <i onClick={() => this.openRules()}
                                className="material-icons settings-button">help_outline</i>
                             {isHost ? (!data.timed
